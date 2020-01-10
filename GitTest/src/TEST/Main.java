bd04670919e5b5ef40d648c3be4d30a88d7b4484
sm09 <sm09@DESKTOP-6A08AK4>
2020-01-10 12:27:52 +0900

    4번쨰

--- GitTest/src/TEST/Main.java
+++ GitTest/src/TEST/Main.java
@@ -7,6 +7,7 @@ public class Main {
                  System.out.println("afjskdjfdjsflkj");
                  System.out.println("러러러러러");
                  System.out.println("ㄴㅇ;ㅏ런이ㅏ러나ㅣㅇ");
+                 System.out.println(ㄹㄹ);
 	}
 
 }
